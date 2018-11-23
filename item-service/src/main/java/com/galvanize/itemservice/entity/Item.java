fd546b6d7741edd264f2aa54fc0ee25c15d10903
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-11-22 20:29:51 -0800

    add hibernate repository to Item-service

--- item-service/src/main/java/com/galvanize/itemservice/entity/Item.java
+++ item-service/src/main/java/com/galvanize/itemservice/entity/Item.java
@@ -1,13 +1,23 @@
 package com.galvanize.itemservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
+@Entity(name = "object")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
